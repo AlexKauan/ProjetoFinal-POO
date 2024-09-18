4be4c375bf6d6094dd3dd08883417a28aae87c94
Jefferson Silva <jefessh@outlook.com>
2024-09-18 20:53:25 -0300

    feat: cria métodos para classe pessoa

--- src/com/project/model/Pessoa.java
+++ src/com/project/model/Pessoa.java
@@ -2,4 +2,15 @@ package com.project.model;
 
 public abstract class Pessoa {
 
+    public abstract void cadastrarPessoa();
+
+    public abstract void mostrarPessoa();
+
+    public abstract void editarPessoa();
+
+    public abstract void listarPessoas();
+
+    public abstract void removerPessoa();
+
+    public abstract boolean logarPessoa(String login, long senha);
 }
