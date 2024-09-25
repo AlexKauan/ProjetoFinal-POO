2c50d04bbcfe3c22732e94fef8781c0a4f991bc9
AlexKauan <alexkauan.dev@gmail.com>
2024-09-25 17:36:25 -0300

    alteração no readme

---
+++ README.md
@@ -0,0 +1,4 @@
+## Como rodar
+
+1. Compile o projeto: `javac -d bin/ src/com/project/controller/VendaController.java`
+2. Execute: `java -cp bin com.project.controller.VendaController`
--- src/com/project/controller/App.java
+++ src/com/project/controller/App.java
@@ -6,7 +6,5 @@ public class App {
     public static void main(String[] args) {
         Cliente pessoa1 = new Cliente("null", "null", "null", "Alex", "null", "null", "null", 12, 1.0);
         System.out.println(pessoa1);
-        System.out.println('Pedro');
-
     }
 }
